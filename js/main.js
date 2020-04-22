40af90f9dc25b9025d7e41273243d5cd2aaa9917
Knowsbleednyahaha <daniel.gayao7@gmail.com>
2020-04-22 10:24:33 +0800

    initial commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$('#item1').click(function(){
+  $(".image-testimonies").css('background', 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .4)), url(img/bohol.jpg)')
+});
+
+$('#item2').click(function(){
+  $(".image-testimonies").css('background', 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .4)), url(img/batangas.jpg)')
+});
+
+$('#item3').click(function(){
+  $(".image-testimonies").css('background', 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .4)), url(img/siquijor.jpg)')
+});
+
+$('#item4').click(function(){
+  $(".image-testimonies").css('background', 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .4)), url(img/cebu.jpg)')
+});
+
+$('#item5').click(function(){
+  $(".image-testimonies").css('background', 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .4)), url(img/elnido.jpg)')
+});
+
+$('#item6').click(function(){
+  $(".image-testimonies").css('background', 'linear-gradient(rgba(0, 0, 0, .3), rgba(0, 0, 0, .4)), url(img/puertoprincesa.jpg)')
+});
+
+
+$('div').click(function(e) {
+    e.preventDefault(); // Can be used anywhere
+});
